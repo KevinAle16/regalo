82859621c98ab67b784cc2f6363fc9ff1a029149
Kevin Fuentes <alejandroftk2@gmail.com>
2025-02-18 12:26:26 -0700

    Se agrego codigo

---
+++ js/partials.js
@@ -0,0 +1,11 @@
+fetch ('partials/head.html')
+.then (response=>response.text())
+.then (data=>document.getElementById('head_contenido').innerHTML=data);
+
+fetch ('partials/header.html')
+.then (response=>response.text())
+.then (data=>document.getElementById('header_contenido').innerHTML=data);
+
+fetch ('partials/footer.html')
+.then (response=>response.text())
+.then (data=>document.getElementById('footer_contenido').innerHTML=data);
